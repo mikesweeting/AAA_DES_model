ee9a1e23dc848797849fba28f12bdcd6301af786
Michael Sweeting <michael.sweeting@le.ac.uk>
2019-02-06 17:04:53 +0000

    add test_model.R

---
+++ test_model.R
@@ -0,0 +1,21 @@
+### Model testing
+
+rm(list=ls())
+
+source("functions/DES Model.R")
+# Parameters.
+source("input/parsForWomen30years_ref_model.R") 
+
+# Settings.
+v0$numberOfPersons <- 1e3
+v0$returnEventHistories <- TRUE 
+v0$recordSizes <- TRUE
+v0$returnAllPersonsQuantities <- TRUE
+v0$method <- "serial"
+v0$randomSeed <- 2
+v0$numberOfProcesses <- detectCores()-1 ## use number cores minus 1 to allow machine to still function
+v0 <- setUnspecifiedElementsOfv0(v0)
+
+# Analysis. 
+systemTimeOutput <- system.time(result <- processPersons(v0, v1other, v2))
+result$meanQuantities
